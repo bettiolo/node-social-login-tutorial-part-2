e8317d914fd09191b2f2dc463c664ca227aa5e9a
Marco Bettiolo <marco@bettiolo.it>
2014-12-17 01:59:30 +0000

    If the user hasn't configured a Name we are going to use the EMail address

--- routes/api.js
+++ routes/api.js
@@ -136,9 +136,9 @@ router.get('/user', function (req, res) {
 	}
 	var userId = getUserIdByToken(token);
 	var user = getUserById(userId);
-	user.name = user.linkedProfiles.google.displayName;
-	user.imageUrl = user.linkedProfiles.google.image.url.replace('?sz=50', '');
 	user.emails = user.linkedProfiles.google.emails.map(function (email) { return email.value });
+	user.name = user.linkedProfiles.google.displayName || user.emails[0];
+	user.imageUrl = user.linkedProfiles.google.image.url.replace('?sz=50', '');
 	user.domain = user.linkedProfiles.google.domain;
 	res.status(200);
 	res.json(user);
