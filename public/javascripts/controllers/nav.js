65a3f89ca51ff16895e85bb302080c01750d10bb
Marco Bettiolo <marco@bettiolo.it>
2014-12-17 01:40:23 +0000

    Fixed ui for new user object format

--- public/javascripts/controllers/nav.js
+++ public/javascripts/controllers/nav.js
@@ -3,7 +3,7 @@ app.controller('navController', function ($scope, $location, loginService) {
 	$scope.login = loginService;
 	$scope.getProfileImageUrl = function () {
 		if ($scope.login.isLoggedIn()) {
-			return $scope.login.user.image.url.replace('sz=50', 'sz=48');
+			return $scope.login.user.imageUrl + '?sz=48';
 		}
 		return 'images/unknown.png';
 	}
